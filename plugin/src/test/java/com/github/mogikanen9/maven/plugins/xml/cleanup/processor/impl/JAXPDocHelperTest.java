1bed0e652f59c4edec229ca3396f5eef690e0a93
Vlad Voinov <mogikanensoftware@gmail.com>
2018-09-17 20:53:35 -0400

    fixed JAXPDocHelper uni test for remove node

--- plugin/src/test/java/com/github/mogikanen9/maven/plugins/xml/cleanup/processor/impl/JAXPDocHelperTest.java
+++ plugin/src/test/java/com/github/mogikanen9/maven/plugins/xml/cleanup/processor/impl/JAXPDocHelperTest.java
@@ -8,7 +8,9 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Source;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactoryConfigurationError;
 
@@ -18,8 +20,17 @@ import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.mockito.Mockito;
+import org.w3c.dom.Attr;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 import org.xml.sax.SAXException;
+import org.xmlunit.builder.DiffBuilder;
+import org.xmlunit.builder.Input;
+import org.xmlunit.diff.DefaultNodeMatcher;
+import org.xmlunit.diff.Diff;
+import org.xmlunit.diff.ElementSelectors;
+import org.xmlunit.util.Convert;
 
 import com.github.mogikanen9.maven.plugins.xml.cleanup.MyMojoTest;
 import com.github.mogikanen9.maven.plugins.xml.cleanup.processor.impl.JAXPDocHelper;
@@ -30,6 +41,10 @@ import com.github.mogikanen9.maven.plugins.xml.cleanup.processor.impl.JAXPDocHel
  *
  */
 public class JAXPDocHelperTest {
+
+	private static final String PROPERTY = "property";
+
+	private static final String ODA_DATA_SOURCE = "oda-data-source";
 
 	private JAXPDocHelper sut;
 
@@ -65,15 +80,69 @@ public class JAXPDocHelperTest {
 		fail("Not yet implemented");
 	}
 
-	@Ignore
 	@Test
-	public void testRemoveNode() {
-		fail("Not yet implemented");
+	public void testRemoveNodeEmptyMock() {
+		sut.removeNode(mock(Node.class));
 	}
 
+	protected Node createPropertyNode(Document doc, String attributeName, String attrValue, String textValue) {
+		
+		Element  tagNode = doc.createElement(PROPERTY);
+		tagNode.setAttribute(attributeName, attrValue);
+		
+		Node tagData = doc.createTextNode(textValue);
+		tagNode.appendChild(tagData);
+		
+		return tagNode;
+	}
+	
+	protected Node createTestDocument(Document doc, String attributeName, String attrValue, String textValue) {
+		Node tagNode = doc.createElement(PROPERTY);
+		
+		Node tagData = doc.createTextNode(textValue);
+		tagNode.appendChild(tagData);
+		
+		Attr nameAtr = doc.createAttribute(attributeName);
+		nameAtr.appendChild(doc.createTextNode(attrValue));
+		
+		tagNode.getAttributes().setNamedItem(nameAtr);
+		
+		return tagNode;
+	}
+	
+	@Test
+	public void testRemoveNodeWithParent() throws ParserConfigurationException {
+		
+		final String controlXml = "<oda-data-source><property name='odaJndi'>JNDI1</property></oda-data-source>";				
+		
+		Document testDoc = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+		.newDocument();
+		
+		Element odaDs = testDoc.createElement(ODA_DATA_SOURCE);
+		
+		
+		Node userNode = createPropertyNode(testDoc, "name", "odaUser","user1");
+		odaDs.appendChild(userNode);
+		
+		Node jndiNode = createPropertyNode(testDoc, "name", "odaJndi","JNDI1");
+		odaDs.appendChild(jndiNode);				
+		
+		testDoc.appendChild(odaDs);
+		
+		sut.removeNode(userNode);
+		
+
+		Diff myDiff = DiffBuilder.compare(Input.fromString(controlXml))
+	              .withTest(Input.fromDocument(testDoc))
+	              .build();
+
+		Assert.assertFalse("XML similar " + myDiff.toString(), myDiff.hasDifferences());
+	}
+
+	
 	@Test
 	public void testSave() throws FileNotFoundException, TransformerFactoryConfigurationError, TransformerException {
-		String destFilePath = validSrcFile.getParentFile().getAbsolutePath()+java.io.File.separator+"destFile.xml";
+		String destFilePath = validSrcFile.getParentFile().getAbsolutePath() + java.io.File.separator + "destFile.xml";
 		sut.save(mock(Document.class), destFilePath);
 		Assert.assertTrue(new File(destFilePath).exists());
 	}
